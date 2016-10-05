905b83aab994399cadeafeca4d80ee8386427cba
Max Liu <zl2480@columbia.edu>
2016-10-05 19:10:24 -0400

    newest version

--- app/global.R
+++ app/global.R
@@ -6,7 +6,7 @@ dt = as.data.frame(fread('./data/NYPD_Felony_2010~2016.csv'))
 
 #checking NA value
 colSums(is.na(dt)) #None
-
+  
 #Split location column for mapping
 dt = dt %>% mutate(lng = substr(dt$`Location 1`, 2, regexpr(',', dt$`Location 1`) - 1)) %>%
   mutate(lat = substr(dt$`Location 1`,regexpr(',', dt$`Location 1`) + 2, nchar(dt$`Location 1`)-1) ) %>%
